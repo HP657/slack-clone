db2ab5664a1f4768a7ba6f136a9abb802426ee45
HP657 <junlee5070@gmail.com>
2024-02-25 19:27:59 +0900

    add db and table and seed data

--- a-nest/src/app.module.ts
+++ a-nest/src/app.module.ts
@@ -8,6 +8,15 @@ import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
 import { UsersService } from './users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChannelChats } from './entities/ChannelChats';
+import { ChannelMembers } from './entities/ChannelMembers';
+import { Channels } from './entities/Channels';
+import { DMs } from './entities/DMs';
+import { Mentions } from './entities/Mentions';
+import { Users } from './entities/Users';
+import { WorkspaceMembers } from './entities/WorkspaceMembers';
+import { Workspaces } from './entities/Workspaces';
 
 @Module({
   imports: [
@@ -15,7 +24,29 @@ import { UsersService } from './users/users.service';
     UsersModule,
     WorkspacesModule,
     ChannelsModule,
-    DmsModule
+    DmsModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [
+        ChannelChats,
+        ChannelMembers,
+        Channels,
+        DMs,
+        Mentions,
+        Users,
+        WorkspaceMembers,
+        Workspaces,
+      ],
+      synchronize: false,
+      keepConnectionAlive: true,
+      logging: true,
+      charset: "utf8mb4"
+    })
   ],
   controllers: [AppController],
   providers: [AppService, UsersService],
